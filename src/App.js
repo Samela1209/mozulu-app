31c79a12915cd385f0e13274ac0df6cbb583462b
Samela1209 <Smkondweni7@gmail.com>
2024-08-24 15:42:45 +0200

    added the github repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Samela Mkondweni and is 
+      <a href="https://github.com/shecodeschool/mozuluapp"
+      target="_blank"> open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
