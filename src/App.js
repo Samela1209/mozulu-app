b7ca9a07347ba1bdb265d31d0f01de1741dfd5a7
Samela1209 <Smkondweni7@gmail.com>
2024-08-24 16:29:41 +0200

    Added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
+      
       <footer>
         This project was coded by Samela Mkondweni and is 
       <a href="https://github.com/shecodeschool/mozuluapp"
       target="_blank"> open-sourced on GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
